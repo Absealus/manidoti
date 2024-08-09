f82e6948fefe2337cbd426468cfadcfe137d5d82
Absealus <andrisblumbergs@gmail.com>
2024-08-09 14:35:10 +0100

    2nd commit

---
+++ mansinstals.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt full-upgrade -y
+
+sudo apt install xinit bspwm polybar kitty picom pulseaudio pulseaudio-module-bluetooth rofi brightnessctl lxpolkit feh dunst vim -y
+
+sudo apt install ufw -y
+sudo ufw enable
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+sudo apt install thunar firefox-esr lxappearance unzip redshift -y
+
+mv ~/mansrepo/manidoti ~/manidoti
+
+rm -rf .config .profile .vim
+
+mkdir -p .config
+
+ln -s $HOME/manidoti/bspwm ~/.config
+ln -s $HOME/manidoti/dunst ~/.config
+ln -s $HOME/manidoti/kitty ~/.config
+ln -s $HOME/manidoti/picom ~/.config
+ln -s $HOME/manidoti/polybar ~/.config
+ln -s $HOME/manidoti/rofi ~/.config
+ln -s $HOME/manidoti/sxhkd ~/.config
+ln -s $HOME/manidoti/Thunar ~/.config
+ln -s $HOME/manidoti/xfce4 ~/.config
+ln -s $HOME/manidoti/vim/vimrc ~/.vimrc
+ln -s $HOME/manidoti/vim/vim ~/.vim
+ln -s $HOME/manidoti/bash/profile ~/.profile
+
+cp ~/manidoti/redshift/redshift.conf ~/.config
+
+sudo cp ~/manidoti/99-synaptics-overrides.conf /etc/X11/xorg.conf.d
+
+chmod +x ~/.config/bspwm/bspwmrc
+chmod +x ~/.config/polybar/launch.sh
+chmod +x ~/.config/polybar/cpu-temp.sh
+
+mkdir -p .themes
+mkdir -p .fonts
+mkdir -p .icons
+
+mv mansrepo .mansrepo
+
+sudo apt purge zutty -y
+sudo apt autoremove --purge -y
