cca84b08219ff3cb02c71a6b917654b407ba2b4f
Absealus <andrisblumbergs@gmail.com>
2024-08-23 18:44:18 +0100

    update

--- mansinstals.sh
+++ mansinstals.sh
@@ -3,21 +3,21 @@
 sudo apt update
 sudo apt full-upgrade -y
 
-sudo apt install xinit bspwm polybar kitty picom pulseaudio pulseaudio-module-bluetooth rofi brightnessctl lxpolkit feh dunst vim -y
+sudo apt install xinit bspwm polybar kitty picom pulseaudio pulseaudio-module-bluetooth rofi brightnessctl lxpolkit feh dunst -y
 
 sudo apt install ufw -y
 sudo ufw enable
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 
-sudo apt install thunar thunar-archive-plugin firefox-esr lxappearance redshift -y
+sudo apt install thunar thunar-archive-plugin firefox-esr geany geany-plugin-addons geany geany-plugin-spellcheck geany-plugin-treebrowser lxappearance redshift -y
 
 sudo apt install network-manager-gnome --no-install-recommends --no-install-suggests -y
 sudo mv /etc/network/interfaces /etc/network/interfaces.bak
 
 sudo apt install blueman -y
 
-rm -rf .config .profile .vim
+rm -rf .config .profile
 
 mkdir -p .config .config/OpenRGB
 
@@ -30,8 +30,7 @@ ln -s $HOME/manidoti/rofi ~/.config
 ln -s $HOME/manidoti/sxhkd ~/.config
 ln -s $HOME/manidoti/Thunar ~/.config
 ln -s $HOME/manidoti/xfce4 ~/.config
-ln -s $HOME/manidoti/vim/.vimrc ~/
-ln -s $HOME/manidoti/vim/.vim ~/
+ln -s $HOME/manidoti/geany ~/.config
 ln -s $HOME/manidoti/openrgb/a.orp ~/.config/OpenRGB/
 ln -s $HOME/manidoti/bash/.profile ~/
 
@@ -48,4 +47,5 @@ mkdir -p .fonts
 mkdir -p .icons
 
 sudo apt purge zutty -y
+sudo apt purge xterm -y
 sudo apt autoremove --purge -y
